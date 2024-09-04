c5d9934979270bd716826cfe98f8b2b47a9245fb
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-09-04 22:44:30 +0300

    Add views for genre and actor

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import GenreList, GenreDetail, ActorList, ActorDetail
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("genres/", GenreList.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actors-detail"),
 ]
 
 app_name = "cinema"
