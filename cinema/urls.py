284b791dc31ff1592fc5a8843874f598dc4cf95a
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-09-04 23:39:30 +0300

    fix link

--- cinema/urls.py
+++ cinema/urls.py
@@ -33,9 +33,9 @@ urlpatterns = [
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actors-detail"),
-    path("cinema-halls/", cinema_hall_list, name="cinema-hall-list"),
+    path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
     path(
-        "cinema-halls/<int:pk>/",
+        "cinema_halls/<int:pk>/",
         cinema_hall_detail,
         name="cinema-hall-detail"
     ),
